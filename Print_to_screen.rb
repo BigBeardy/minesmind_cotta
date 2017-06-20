f4e1651a9513801346c0e295180f06e65630ff5a
BigBeardy <westderick70@gmail.com>
2017-06-20 17:00:58 -0400

    so damn close

--- Print_to_screen.rb
+++ Print_to_screen.rb
@@ -1,2 +1,19 @@
-print ["Hello world"]
-(1..100).each { |n| puts n }
+# print ["Hello world"] 
+# p (1..100).to_a
+# if 3 
+# 	then 
+# 	print "mined"
+# 	end
+
+
+x=0
+100.times do 
+if x == 3
+	p "mined"
+	end
+	x+=1
+	p x
+end
+
+
+	
