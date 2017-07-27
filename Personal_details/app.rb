5906fe2ef6bea0228f1d1b9151780d3f2871b392
BigBeardy <westderick70@gmail.com>
2017-07-27 15:37:21 -0400

    moving along

--- Personal_details/app.rb
+++ Personal_details/app.rb
@@ -13,14 +13,16 @@ end
 
 post '/name' do 
 	name = params[:user_name]
-	last_name = params[:last_name]
-	redirect '/age?user_name=' + name + '&last_name=' + last_name
+	# user_lastname = params[:last_name]
+	redirect '/age?user_name=' + name 
+	# '&last_name=' + last_name 
 end
 
 get '/age' do 
 	name = params[:user_name]
-	last_name = params[:last_name]
-	erb :get_age, :locals => {:name => name, :last_name => last_name} 
+	# last_name = params[:last_name]
+	erb :get_age, :locals => {:name => name, } 
+	# :last_name => last_name
 end
 post '/age' do 
 	age = params[:user_age]
@@ -47,7 +49,7 @@ get '/hair_color' do
 	fav_num1 = params[:user_number_1] 
 	fav_num2 = params[:user_number_2]
 	fav_num3 = params[:user_number_3]
-	erb :hair_color, :locals => {:name => name, :age => age, :user_number_1 => fav_num1 , :user_number_2 => fav_num2, :user_number_3 => fav_num3 }
+	erb :hair_color, :locals => {:name => name, :age => age, :fav_num1 => fav_num1 , :fav_num2 => fav_num2, :fav_num3 => fav_num3 }
 	end
 
 post '/hair_color' do 
@@ -56,7 +58,47 @@ post '/hair_color' do
 	fav_num1 = params[:user_number_1] 
 	fav_num2 = params[:user_number_2]
 	fav_num3 = params[:user_number_3]
-	redirect '/fav_food?user_name=' + name + '&user_age=' + age + '&user_number_1=' + fav_num1 + '&user_number_2=' + fav_num2 + '&user_number_3=' + fav_num3 + '&user_hair=' + user_hair
+	hair = params[:user_hair]
+	redirect '/fav_food?user_name=' + name + '&user_age=' + age + '&user_number_1=' + fav_num1 + '&user_number_2=' + fav_num2 + '&user_number_3=' + fav_num3 + '&user_hair=' + hair
 end
 get '/fav_food' do
+	age = params[:user_age]
+	name = params[:user_name]
+	fav_num1 = params[:user_number_1] 
+	fav_num2 = params[:user_number_2]
+	fav_num3 = params[:user_number_3]
+	hair = params[:user_hair]
+	erb :fav_food, :locals => {:name => name, :age => age, :fav_num1 => fav_num1 , :fav_num2 => fav_num2, :fav_num3 => fav_num3, :hair => hair }
+	end
+
+post '/fav_food' do
+	age = params[:user_age]
+	name = params[:user_name]
+	fav_num1 = params[:user_number_1] 
+	fav_num2 = params[:user_number_2]
+	fav_num3 = params[:user_number_3]
+	hair = params[:user_hair]
+	food = params[:fav_food]
+	redirect '/eyecolor?user_name=' + name + '&user_age=' + age + '&user_number_1=' + fav_num1 + '&user_number_2=' + fav_num2 + '&user_number_3=' + fav_num3 + '&user_hair=' + hair + '/fav_food' + food
+end
+
+ get '/eyecolor' do 
+ 	age = params[:user_age]
+	name = params[:user_name]
+	fav_num1 = params[:user_number_1] 
+	fav_num2 = params[:user_number_2]
+	fav_num3 = params[:user_number_3]
+	hair = params[:user_hair]
+	food = params[:fav_food]
+	erb :eyecolor, :locals => {:name => name, :age => age, :fav_num1 => fav_num1 , :fav_num2 => fav_num2, :fav_num3 => fav_num3, :hair => hair, :food => food }
+	end
+post '/eyecolor' do 
+	age = params[:user_age]
+	name = params[:user_name]
+	fav_num1 = params[:user_number_1] 
+	fav_num2 = params[:user_number_2]
+	fav_num3 = params[:user_number_3]
+	hair = params[:user_hair]
+	food = params[:fav_food]
+	redirect '/final?user_name=' + name + '&user_age=' + age + '&user_number_1=' + fav_num1 + '&user_number_2=' + fav_num2 + '&user_number_3=' + fav_num3 + '&user_hair=' + hair + '&fav_food' + food
 end
